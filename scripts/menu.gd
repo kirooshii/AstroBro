2d4f5a3453005060354922563d4d2e78700f742d
Makar Lavrov <makarlavrov2005@gmail.com>
2024-10-05 14:43:43 +0400

    almost ready main menu

---
+++ scripts/menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+
+func _on_options_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/options.tscn")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit( )
