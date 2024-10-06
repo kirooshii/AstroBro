58e9a89a53d79ab0f25da2ec608a5e03f36d3cf1
Makar Lavrov <makarlavrov2005@gmail.com>
2024-10-06 12:39:28 +0400

    main menu fix: options now working

--- scripts/main-menu/main-menu.gd
+++ scripts/main-menu/main-menu.gd
@@ -7,7 +7,7 @@ extends Control
 
 #click button to go the settings
 func _on_options_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/options.tscn")
+	get_tree().change_scene_to_file("res://scenes/main-menu/options.tscn")
 	
 #click button to exit the game
 func _on_exit_pressed() -> void:
--- scripts/main-menu/options.gd
+++ scripts/main-menu/options.gd
@@ -2,4 +2,4 @@ extends Control
 
 #click button to return to main menu
 func _on_back_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/main-menu.tscn")
+	get_tree().change_scene_to_file("res://scenes/main-menu/main-menu.tscn")
